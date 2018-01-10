accd81846765aa58044835a7e90ed889fa63bace
Gio on ArchXPS <github@gpiantoni.com>
2018-01-10 19:54:25 +0100

    use simulated bids

--- tests/paths.py
+++ tests/paths.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-PATH_ROOT = Path(__file__).resolve().parent.parent
-PATH_BIDS = PATH_ROOT / 'BIDS-examples'
-PATH_IEEG = PATH_BIDS / 'ieeg_visual'
+TEST_PATH = Path(__file__).resolve().parent
+DATA_PATH = TEST_PATH / 'data'
+BIDS_PATH = DATA_PATH / 'bids'
