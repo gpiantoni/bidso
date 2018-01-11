d0915f42b2a6ddabee90cdfd9a5d1c6376de4b15
Gio on ArchXPS <github@gpiantoni.com>
2018-01-11 15:49:04 +0100

    create mri data

--- tests/test_01_simulate.py
+++ tests/test_01_simulate.py
@@ -1,11 +1,12 @@
 from json import dump
 from nibabel import load as nload
+from nibabel import Nifti1Image
 
 from bidso.simulate.fmri import create_bold, create_events
 from bidso.utils import add_underscore
 from bidso import Task
 
-from .paths import BIDS_PATH
+from .paths import BIDS_PATH, FREESURFER_PATH
 
 subject = 'bert'
 session = 'day01'
@@ -21,6 +22,8 @@ modality_path = sess_path / f'{modality}'
 modality_path.mkdir()
 fmri_file = modality_path / f'sub-{subject}_ses-{session}_task-block_run-00'
 
+T1_path = FREESURFER_PATH / 'bert/mri/T1.mgz'
+
 
 def test_simulate_root():
 
@@ -31,8 +34,19 @@ def test_simulate_root():
         f.write(f'{subject}\t30\tF\n')
 
 
+def test_simulate_anat():
+
+    mri = nload(str(T1_path))
+    x = mri.get_data()
+    nifti = Nifti1Image(x, mri.affine)
+
+    anat_path = BIDS_PATH / f'sub-{subject}/ses-{session}/anat/'
+    anat_path.mkdir(parents=True)
+    nifti.to_filename(str(anat_path / f'sub-{subject}_T1w.nii.gz'))
+
+
 def test_simulate_fmri():
-    mri = nload('tests/data/derivatives/freesurfer/bert/mri/T1.mgz')  # TODO
+    mri = nload(str(T1_path))
 
     create_bold(mri, add_underscore(fmri_file, 'bold.nii.gz'))
     create_events(add_underscore(fmri_file, 'events.tsv'))
