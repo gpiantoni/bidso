74ea6c27bd83deb99b7b78cb07e649268d2c9e1c
Gio at UMCU <github@gpiantoni.com>
2018-01-17 15:56:50 +0100

    use file_Core instead of namedtuple

--- tests/paths.py
+++ tests/paths.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from functools import namedtuple
+from bidso import file_Core
 
 
 TEST_PATH = Path(__file__).resolve().parent
@@ -10,12 +10,22 @@ DERIVATIVES_PATH = DATA_PATH / 'derivatives'
 FREESURFER_PATH = DERIVATIVES_PATH / 'freesurfer'
 
 
-# this has a similar signature to Task, but Task is tested later
-TaskSimple = namedtuple('BIDS', ('subject', 'session', 'modality', 'task', 'run'))
-
 subject = 'bert'
-task_fmri = TaskSimple(subject, 'day01', 'func', 'block', '00')
-task_anat = TaskSimple(subject, 'day01', 'anat', 'block', '00')
-task_ieeg = TaskSimple(subject, 'day02', 'ieeg', 'block', '00')
+task_ieeg = file_Core(
+    subject=subject,
+    session='day02',
+    modality='ieeg',
+    task='block',
+    run='00')
+task_fmri = file_Core(
+    subject=subject,
+    session='day01',
+    modality='func',
+    task='block',
+    run='00')
+task_anat = file_Core(
+    subject=subject,
+    session='day01',
+    modality='anat')
 
 T1_PATH = FREESURFER_PATH / 'sub-bert/mri/T1.mgz'
